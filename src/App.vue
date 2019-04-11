6dc18a2beb6e27fe006ec5aea937d926bd3ac597
Klement Baastrup Johansen <kljo@campusvejle.dk>
2019-04-11 19:20:02 +0200

    init 😎

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-container>
+    <LoginForm id="form"></LoginForm>
+  </b-container>
+</template>
+
+<script>
+import LoginForm from './components/LoginForm.vue';
+
+export default {
+  name: 'app',
+  components: {
+    LoginForm
+  }
+}
+</script>
+
+<style>
+#form {
+  margin: 10rem auto;
+  padding: 2rem;
+  max-width: 400px;
+  /* border: 1px solid #ddd; */
+  /* box-shadow: 1px 1px 10px #ddd; */
+}
+</style>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,65 @@
+<template>
+  <b-form @submit="onSubmit" v-if="show" id="form">
+    <!-- Email -->
+    <b-form-group
+      id="input-group-email"
+      label="Email"
+      label-for="input-email"
+    >
+      <b-form-input
+        id="input-email"
+        v-model="form.email"
+        required
+        placeholder="Enter email"
+      ></b-form-input>
+    </b-form-group>
+    
+    <!-- Password -->
+    <b-form-group
+      id="input-group-password"
+      label="Password"
+      label-for="input-password"
+    >
+      <b-form-input
+        id="input-password"
+        type="password"
+        v-model="form.password"
+        required
+        placeholder="Enter password"
+      ></b-form-input>
+    </b-form-group>
+
+    <!-- Submit button -->
+    <b-button type="submit" variant="primary">Submit</b-button>
+  </b-form>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm',
+
+  data() {
+    return {
+      form: {
+        email: '',
+        password: ''
+      },
+      show: true
+    }
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault()
+      alert(JSON.stringify(this.form))
+    }
+  },
+  props: {
+    // msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+
+</style>
