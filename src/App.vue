71cff5d10c4b4ab39749c0bd90a30b9ad1ec5973
Klement Baastrup Johansen <kljo@campusvejle.dk>
2019-04-24 13:20:59 +0200

    Add router-view to app

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,33 @@
 <template>
-  <b-container>
-    <LoginForm id="form"></LoginForm>
+<div>
+  <TabBar />
+  <b-container class="mt-5">
+    <router-view></router-view>
   </b-container>
+</div>
 </template>
 
 <script>
-import LoginForm from './components/LoginForm.vue';
+import TabBar from './components/TabBar'
 
 export default {
   name: 'app',
   components: {
-    LoginForm
+    TabBar
   }
 }
 </script>
 
 <style>
+
 #form {
-  margin: 10rem auto;
+  margin: 5rem auto;
   padding: 2rem;
   max-width: 400px;
+  border: 0;
+  border-radius: .3rem;
+  background-color: white;
+  box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
   /* border: 1px solid #ddd; */
   /* box-shadow: 1px 1px 10px #ddd; */
 }
