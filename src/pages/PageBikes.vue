478d4b3f2f241410822164e81cb3642bbc7f0a1f
Klement Baastrup Johansen <kljo@campusvejle.dk>
2019-04-28 23:52:42 +0200

    Add more information to PageBike and PageUSer

--- src/pages/PageBikes.vue
+++ src/pages/PageBikes.vue
@@ -1,9 +1,38 @@
 <template>
+<div>
   <h1>PageBikes</h1>
+  <AddBike />
+  <b-card class="mt-3" header="All bikes">
+    <pre class="m-0">{{ allBikes }}</pre>
+  </b-card>
+  <b-table responsive hover :items="allBikes" :fields="fields"></b-table>
+</div>
 </template>
 
 <script>
+import AddBike from '@/components/AddBike'
+
 export default {
-  name: 'PageBikes'
+  name: 'PageBikes',
+
+  components: {
+    AddBike
+  },
+
+  data() {
+    return {
+      bikes: []
+    }
+  },
+
+  created: function() {
+    this.$store.dispatch('bike/fetchAllBikes')
+  },
+
+  computed: {
+    allBikes() {
+      return this.$store.getters['bike/allBikes']
+    }
+  }
 }
 </script>
--- src/pages/PageUser.vue
+++ src/pages/PageUser.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
-    <h1>PageUser</h1>
-    <UsersList />
+      <h1 class="ml-1">Users</h1>
+      <UsersList />
+      <!-- <UserEdit /> -->
   </div>
 </template>
 
 <script>
+// import UserEdit from '@/components/UserEdit'
 import UsersList from '@/components/UsersList'
 
 export default {
   name: 'PageUser',
 
   components: {
+    // UserEdit,
     UsersList
   }
 }
