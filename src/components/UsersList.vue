38c7d1617025201afdb252b3e5c758cd3d20be4f
Klement Baastrup Johansen <kljo@campusvejle.dk>
2019-04-28 23:52:15 +0200

    UserList working with store

--- src/components/UsersList.vue
+++ src/components/UsersList.vue
@@ -1,5 +1,14 @@
 <template>
-  <b-table responsive hover :items="users" :fields="fields"></b-table>
+<div>
+
+  <!-- <b-table responsive hover :items="allUsers" :fields="fields"></b-table> -->
+  <b-card class="mt-3" header="Current User">
+    <pre class="m-0">{{ user }}</pre>
+  </b-card>
+  <!-- <b-card class="mt-3" header="All Users">
+    <pre class="m-0">{{ allUsers }}</pre>
+  </b-card> -->
+</div>
 </template>
 
 <script>
@@ -15,10 +24,19 @@ export default {
         { first_name: 'Jane', last_name: 'Doe', email: 'Jane@doe.com', phone: '39872738', role: 'manager'}
       ]
     }
+  },
+  created: function() {
+    this.$store.dispatch('user/fetchCurrentUser')
+    this.$store.dispatch('user/fetchAllUsers')
+    // users = user()
+  },
+  computed: {
+    user() {
+      return this.$store.getters['user/currentUser']
+    },
+    allUsers() {
+      return this.$store.getters['user/allUsers']
+    }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
