61e580a9d794668cd1bc25689ed66cb24accb04c
Klement Baastrup Johansen <kljo@campusvejle.dk>
2019-04-28 23:51:09 +0200

    Add user and bike module to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import * as auth from './modules/auth'
+import * as user from './modules/user'
+import * as bike from './modules/bike'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    auth
+    auth,
+    user,
+    bike
   }
 })
