6f8e731f72b4bd750b223df23c17b88818b20f7a
Klement Baastrup Johansen <kljo@campusvejle.dk>
2019-05-15 12:29:13 +0200

    Add vuetify, remove bootstrap

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import store from './store/store'
 import router from './router'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 import './custom.scss'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
