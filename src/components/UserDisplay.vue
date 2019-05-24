7c156666614cc1ba884aae91687bf80ea8b49128
Mikkel Boese Johnsen <mikk571a@edu.campusvejle.dk>
2019-05-24 09:36:10 +0200

    ÃÃchanged component UserDisplay to use computed property instead and to not hold any variables of the user

--- src/components/UserDisplay.vue
+++ src/components/UserDisplay.vue
@@ -3,11 +3,11 @@
     <v-list class="pa-0">
       <v-list-tile avatar>
         <v-list-tile-avatar>
-          <img :src="getPicture()" alt="avatar">
+          <img :src="getImg !== null ? 'https://imgplaceholder.com/420x320/3a47c6/f7f7f7/glyphicon-user' : getimg " alt="avatar">
         </v-list-tile-avatar>
 
         <v-list-tile-content>
-          <v-list-tile-title>{{getName()}}</v-list-tile-title>
+          <v-list-tile-title>{{ fullName }}</v-list-tile-title>
         </v-list-tile-content>
       </v-list-tile>
     </v-list>
@@ -16,37 +16,16 @@
 
 <script>
 export default {
-  name: 'UserDisplay',          
-    data() {
-        return{
-        user: {}
-        }
-    },
-  methods: {     
-    
-    
-     //getting the name from the user and making sure there is something in it
-    getName() {
-          this.user = this.$store.state.user.currentUser
-          
-          if(!this.user.firstName || !this.user.lastName || this.user.firstName === "" || this.user.lastName === "" || !this.user || this.user == {} ){
-            return "Placeholder Holdersen Place"
-          }
-          else{
-            return this.user.firstName + " " +  this.user.lastName
-       }
+  name: 'UserDisplay',
+
+  computed: {
+
+    fullName() {
+      return this.$store.state.user.currentUser.firstName + " " + this.$store.state.user.currentUser.lastName
     },
 
-    //getting picture for the avatar
-    getPicture() {
-      this.user = this.$store.state.user.currentUser
-      
-      if(!this.user.image || this.user.image === "" || !this.user || this.user == {} ){
-            return "https://imgplaceholder.com/420x320/3a47c6/f7f7f7/glyphicon-user"
-          }
-          else{
-            return this.user.image      
-      }  
+    getImg(){
+      return this.$store.state.user.currentUser.img
     }
   }
 }
