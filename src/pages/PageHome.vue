2c3c563a7bc985fccaa993e661cd4cc2ec0c729a
Klement Baastrup Johansen <klementj@gmail.com>
2019-05-28 21:17:03 +0200

    Removed page headings

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,12 +1,9 @@
 <template>
-<div>
-  <h1>Home</h1>
-
-  <v-btn @click="testNotification({ message: 'Notification uden sat farve'})">Standard</v-btn>
-  <v-btn @click="testNotification({ message: 'Hurra. Du har vundet', color: 'success'})">Primary color</v-btn>
-  <v-btn @click="testNotification({ message: 'Fejl... Fej--', color: 'error'})">Error color</v-btn>
-</div>
-
+  <div>
+    <v-btn @click="testNotification({ message: 'Notification uden sat farve'})">Standard</v-btn>
+    <v-btn @click="testNotification({ message: 'Hurra. Du har vundet', color: 'success'})">Primary color</v-btn>
+    <v-btn @click="testNotification({ message: 'Fejl... Fej--', color: 'error'})">Error color</v-btn>
+  </div>
 </template>
 
 <script>
