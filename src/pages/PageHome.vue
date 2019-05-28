0e320cca590042ed49c69cf1f956f0f88a561f5d
Klement Baastrup Johansen <klementj@gmail.com>
2019-05-28 11:56:55 +0200

    Included component on pageHome for testing

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -2,6 +2,10 @@
 <div>
   <h1>Home</h1>
 
+  <!-- <UploadBtn /> -->
+
+  <ImageUpload />
+
   <v-btn @click="testNotification({ message: 'Notification uden sat farve'})">Standard</v-btn>
   <v-btn @click="testNotification({ message: 'Hurra. Du har vundet', color: 'success'})">Primary color</v-btn>
   <v-btn @click="testNotification({ message: 'Fejl... Fej--', color: 'error'})">Error color</v-btn>
@@ -10,8 +14,16 @@
 </template>
 
 <script>
+// import UploadBtn from '@/components/UploadButton'
+import ImageUpload from '@/components/ImageUpload'
+
 export default {
   name: 'PageHome',
+
+  components: {
+    // UploadBtn,
+    ImageUpload
+  },
 
   methods: {
     testNotification(payload) {
