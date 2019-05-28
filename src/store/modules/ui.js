8c9cef706d01663d668ed4e975163e0f25567848
Klement Baastrup Johansen <klementj@gmail.com>
2019-05-28 20:19:53 +0200

    Add vuex mutation to toggle navigation drawer

---
+++ src/store/modules/ui.js
@@ -0,0 +1,12 @@
+export const namespaced = true
+
+export const state = {
+  drawer: null,
+}
+
+export const mutations = {
+  // Toogle navigation drawer
+  SET_DRAWER(state, bool) {
+    state.drawer = bool
+  }
+}
