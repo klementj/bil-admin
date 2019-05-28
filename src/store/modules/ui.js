0fecc62cfe51e9ad23af7335b01a6d0c18a01c56
Klement Baastrup Johansen <klementj@gmail.com>
2019-05-28 20:25:46 +0200

    Add toolbar title state and mutation

--- src/store/modules/ui.js
+++ src/store/modules/ui.js
@@ -2,11 +2,17 @@ export const namespaced = true
 
 export const state = {
   drawer: null,
+  toolbarTitle: ''
 }
 
 export const mutations = {
   // Toogle navigation drawer
   SET_DRAWER(state, bool) {
     state.drawer = bool
+  },
+
+  // Setting title on toolbar
+  SET_TOOLBAR_TITLE(state, title) {
+    state.toolbarTitle = title
   }
 }
