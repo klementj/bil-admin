07d9289a11b34f6321511a97476fa4142f9da62c
Mikkel Boese Johnsen <mikk571a@edu.campusvejle.dk>
2019-05-28 12:01:29 +0200

    made datatableuser out of from datatable as template

---
+++ src/components/DataTableUser.vue
@@ -0,0 +1,61 @@
+<template>
+    <v-card>
+        <v-card-title>
+            <h2> Brugere </h2>
+        <v-spacer></v-spacer>
+        <v-text-field
+            v-model="search"
+            label="Search"
+            single-line
+            hide-details
+        ></v-text-field>
+        </v-card-title>    
+        <v-data-table
+        :headers="header"
+        :items="user"
+        :search="search"
+        >
+            <template v-slot:items="user">
+                <td class="text-xs-left">{{user.item.firstName}}</td>
+                <td class="text-xs-right">{{user.item.lastName}}</td>
+                <td class="text-xs-right">{{user.item.email}}</td>
+                <td class="text-xs-right">{{user.item.phone}}</td>
+                <td class="text-xs-right">{{user.item.bookings}}</td>
+                <td class="text-xs-right">{{user.item.role}}</td>
+            </template>
+            <template v-slot:no-results>
+                <v-alert :value="true" color="error" icon="warning">
+                Your search for "{{ search }}" found no results.
+            </v-alert>
+            </template>
+        </v-data-table>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: "DataTableUser",
+    
+    props: {
+        user: Array
+    },
+
+    data() {
+        return{
+            search: '',
+            header: [
+                { text: 'Navn', value: 'firstName' },
+                { text: 'Efternavn', value: 'lastName' },
+                { text: 'Email', value:'email' },
+                { text: 'Cykel lån', value: 'bookings' },
+                { text: 'tlf.', value: 'phone' },
+                { text: 'bruger rolle', value: 'role' }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/PageUser.vue
+++ src/pages/PageUser.vue
@@ -2,6 +2,7 @@
   <div>
       <h1 class="ml-1">Users</h1>
       <UsersList />
+      <DataTableUser :user="allUsers" ></DataTableUser>
       <!-- <UserEdit /> -->
   </div>
 </template>
@@ -9,13 +10,30 @@
 <script>
 // import UserEdit from '@/components/UserEdit'
 import UsersList from '@/components/UsersList'
+import DataTableUser from '@/components/DataTableUser'
+import { promises } from 'fs';
+import { Promise } from 'q';
 
 export default {
   name: 'PageUser',
 
   components: {
     // UserEdit,
-    UsersList
+    UsersList,
+    DataTableUser
+  },
+
+  methods: {
+    getUsers(){
+
+    }
+  },
+
+  computed: {    
+    allUsers() {
+      const users =  Promise.resolve(this.$store.dispatch('user/fetchAllUsers'))
+      return users
+    }
   }
 }
 </script>
