13b1228246ebb3075d9c500ffea678b3230f371c
Klement Baastrup Johansen <klementj@gmail.com>
2019-05-28 20:20:50 +0200

    Add method to toggle drawer state in toolbar

--- src/pages/PageMain.vue
+++ src/pages/PageMain.vue
@@ -1,28 +1,35 @@
 <template>
-    <v-app>
-        <navigation />
+  <v-app>
+    <Navigation />
 
-        <v-toolbar color="primary" dark fixed app>
-            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-            <v-toolbar-title>Bicycle Library</v-toolbar-title>
-        </v-toolbar>
+    <v-toolbar color="primary" dark fixed app>
+      <v-toolbar-side-icon @click.stop="toggleDrawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Bicycle Library</v-toolbar-title>
+    </v-toolbar>
 
-        <v-content fill-height>
-            <v-container>
-                <router-view></router-view>
-            </v-container>
-        </v-content>
-    </v-app>
+    <v-content fill-height>
+        <v-container>
+            <router-view></router-view>
+        </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 import Navigation from '../components/Navigation.vue'
 
 export default {
-    name: 'Main',
-    components:{
-        Navigation
-    },
+  name: 'Main',
+
+  components: {
+      Navigation
+  },
+
+  methods: {
+    toggleDrawer: function() {
+      this.$store.state.ui.drawer = !this.$store.state.ui.drawer
+    }
+  }
 }
 </script>
 
