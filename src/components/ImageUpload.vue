95b847348c28e5c8c78c8ebbaab035e68f89b4b6
Klement Baastrup Johansen <klementj@gmail.com>
2019-05-28 11:56:17 +0200

    Image upload component

---
+++ src/components/ImageUpload.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <input type="file" @change="onfileSelected">
+    <button @click="uploadFile">Upload</button>
+  </div>
+</template>
+
+<script>
+import ImageService from '@/services/image.service'
+import Compressor from 'compressorjs'
+
+export default {
+  name: 'ImageUpload',
+
+  data () {
+    return {
+      selectedFile: null
+    }
+  },
+
+  methods: {
+    onfileSelected(event) {
+      this.selectedFile = event.target.files[0]
+    },
+    uploadFile() {
+      if(this.selectedFile === null) {
+        return
+      }
+      
+      const imageService = new ImageService
+
+      // Initial compression to make sure we don't upload huge images.
+      new Compressor(this.selectedFile, {
+        // default quality
+        quality: 0.8,
+        // Ipad retina screensize
+        maxWidth: 2732,
+        maxHeight: 2048,
+        success(result){
+          const formData = new FormData()
+          formData.append('file', result, result.name)
+          formData.append('title', 'En titel til billedet')
+          formData.append('alt', 'En titel til billedet')
+
+          imageService.create(formData)
+            .then(() => {
+              // eslint-disable-next-line
+              console.log('Upload success');
+            })
+        },
+        error(error) {
+          // eslint-disable-next-line
+          console.log(error.message)
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
