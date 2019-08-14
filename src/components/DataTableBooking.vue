131f3c76e33a16a3a5e2480efbfb056573f557de
Daniel Nicolas Birk Holm <dani479d@edu.campusvejle.dk>
2019-08-14 12:29:07 +0200

    Created bookingList component and added it to bookingPage. The list is working but still needs to display the correct information about the booking.

---
+++ src/components/DataTableBooking.vue
@@ -0,0 +1,60 @@
+<template>
+    <v-card>
+        <v-card-title>
+            <h2> Bookings </h2>
+        <v-spacer></v-spacer>
+        <v-text-field
+            v-model="search"
+            label="Search"
+            single-line
+            hide-details
+        ></v-text-field>
+        </v-card-title>    
+        <v-data-table
+        :headers="header"
+        :items="bookings"
+        :search="search"
+        >
+            <template v-slot:items="bookings">
+                <td class="text-xs-left">{{bookings.item.user}}</td>
+                <td class="text-xs-right">{{bookings.item.bike}}</td>
+                <td class="text-xs-right">{{bookings.item.startTime}}</td>
+                <td class="text-xs-right">{{bookings.item.endTime}}</td>
+            </template>
+            <template v-slot:no-results>
+                <v-alert :value="true" color="error" icon="warning">
+                Your search for "{{ search }}" found no results.
+            </v-alert>
+            </template>
+        </v-data-table>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: "DataTableBooking",
+    
+    props: {
+        bookings: Array
+    },
+
+    data() {
+        return{
+            search: '',
+            header: [
+                { text: 'User', align: 'left' , value: 'user' },
+                { text: 'Bike', align: 'right' , value: 'bike' },
+                { text: 'Start', align: 'right' , value:'startTime' },
+                { text: 'End', align: 'right' , value: 'endTime' }
+            ]
+        }
+    },
+
+    computed: {
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/PageBookings.vue
+++ src/pages/PageBookings.vue
@@ -1,21 +1,32 @@
 <template>
+<div>
   <Booking />
+  <DataTableBooking :bookings="allBookings" />
+</div>
 </template>
 
 <script>
 import Booking from '@/components/CreateBooking.vue'
+import DataTableBooking from '@/components/DataTableBooking.vue'
 
 export default {
   name: 'PageBookings',
 
   components: {
-    Booking
+    Booking,
+    DataTableBooking
   },
 
   created: function (){
-            this.$store.dispatch('bike/fetchAllBikes');
-            this.$store.dispatch('user/fetchAllUsers');
-            this.$store.dispatch('booking/fetchAllBookings');
-        }
+    this.$store.dispatch('bike/fetchAllBikes');
+    this.$store.dispatch('user/fetchAllUsers');
+    this.$store.dispatch('booking/fetchAllBookings');
+  },
+
+  computed: {
+    allBookings: function(){
+      return this.$store.getters['booking/allBookings'];
+    }
+  }
 }
 </script>
