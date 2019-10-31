1ed25130faf12c94db70d69b54907360a7dcbcba
witwitwut <33252130+witwitwut@users.noreply.github.com>
2019-10-31 11:28:48 +0100

    Update UserDisplay.vue

--- src/components/UserDisplay.vue
+++ src/components/UserDisplay.vue
@@ -3,9 +3,9 @@
       <v-list class="pa-0">
         <v-list-item>
           <v-list-item-avatar>
-          </v-list-item-avatar>
             <v-img :src="getImg !== null ? 'https://imgplaceholder.com/420x320/3a47c6/f7f7f7/glyphicon-user' : getimg " alt="avatar"></v-img>
-          <v-list-item-content>
+          </v-list-item-avatar>
+            <v-list-item-content>
             <v-list-item-title>{{ fullName }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
