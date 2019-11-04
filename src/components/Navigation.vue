cb43751bb858ec11ca834e8e46129fec5a8fbdee
Klement Baastrup Johansen <kljo@campusvejle.dk>
2019-11-04 17:18:38 +0100

    Update navigation-drawer to vuetify2

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,66 +1,63 @@
 <template>
   <v-navigation-drawer v-model="drawer" fixed app>
-    <v-layout justify-space-between column fill-height>
-      <!-- Dispay current users profile picture and name -->
-      
-      <v-list class="pt-0">
-        <UserDisplay />
-        <v-divider></v-divider>
 
-        <v-list-item :to="{ name: 'home'}">
-          <v-list-item-action>
-            <v-icon>mdi-view-dashboard</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Dashboard</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+    <UserDisplay />
+    <v-divider></v-divider>
 
-        <v-list-item :to="{ name: 'bookings'}">
-          <v-list-item-action>
-            <v-icon>mdi-calendar</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Bookings</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+    <v-list class="pt-0">
+      <v-list-item :to="{ name: 'home'}">
+        <v-list-item-action>
+          <v-icon>mdi-view-dashboard</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Dashboard</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
 
-        <v-list-item :to="{ name: 'bikes'}">
-          <v-list-item-action>
-            <v-icon>mdi-bike</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Bikes</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+      <v-list-item :to="{ name: 'bookings'}">
+        <v-list-item-action>
+          <v-icon>mdi-calendar</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Bookings</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
 
-        <v-list-item :to="{ name: 'users'}">
-          <v-list-item-action>
-            <v-icon>mdi-account-group</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Users</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+      <v-list-item :to="{ name: 'bikes'}">
+        <v-list-item-action>
+          <v-icon>mdi-bike</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Bikes</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
 
-        <v-list-item :to="{ name: 'projects'}">
-          <v-list-item-action>
-            <v-icon>mdi-sunglasses</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Projects</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+      <v-list-item :to="{ name: 'users'}">
+        <v-list-item-action>
+          <v-icon>mdi-account-group</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Users</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
 
-      <div class="pa-2 mt-auto">
-        <ToggleTheme />
-      </div>
+      <v-list-item :to="{ name: 'projects'}">
+        <v-list-item-action>
+          <v-icon>mdi-sunglasses</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Projects</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+
+    <template v-slot:append>
+      <ToggleTheme class="pa-2"/>
 
       <div class="pa-2">
         <v-btn block outlined color="primary" :to="{ name: 'logout'}">Logout</v-btn>
       </div>
-    </v-layout>  
+    </template>
   </v-navigation-drawer>
 </template>
 
