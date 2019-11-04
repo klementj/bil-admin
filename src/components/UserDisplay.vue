f98386096ea203471352954dc32f4d9945e16766
Klement Baastrup Johansen <kljo@campusvejle.dk>
2019-11-04 17:18:10 +0100

    Add a subtitle to user display

--- src/components/UserDisplay.vue
+++ src/components/UserDisplay.vue
@@ -1,19 +1,17 @@
-<template>
-    <v-toolbar flat class="transparent">
-      <v-list class="pa-0">
-        <v-list-item>
-          <v-list-item-avatar>
-            <v-img v-if=" typeof getImg === 'undefined' || getImg === null" src="@/assets/person.svg" >
-            </v-img>
-            <v-img v-else :src="getImg">
-            </v-img>
-          </v-list-item-avatar>
-           <v-list-item-content>
-            <v-list-item-title>{{ fullName }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-toolbar>
+<template v-slot:prepend>
+  <v-list-item two-line>
+    <v-list-item-avatar>
+      <v-img v-if=" typeof getImg === 'undefined' || getImg === null" src="@/assets/person.svg" >
+      </v-img>
+      <v-img v-else :src="getImg">
+      </v-img>
+    </v-list-item-avatar>
+
+    <v-list-item-content>
+      <v-list-item-title>{{ fullName }}</v-list-item-title>
+      <v-list-item-subtitle>Logged In</v-list-item-subtitle>
+    </v-list-item-content>
+  </v-list-item>
 </template>
 
 <script>
